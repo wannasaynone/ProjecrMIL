00b81af73e384f7e21757f36129dca296f843d7a
KAHA <wannasaynone@hotmail.com>
2024-05-11 21:26:18 +0800

    add UI assets

--- Assets/Scripts/Adventure/AdventureManager.cs
+++ Assets/Scripts/Adventure/AdventureManager.cs
@@ -99,7 +99,7 @@ namespace ProjectMIL.Adventure
                 jackpotCount++;
                 jackpotRate = 0f;
             }
-            else if (totalCount >= 8 + jackpotCount * 20 && jackpotCount < 3 && addGold <= 100)
+            else if (totalCount >= 5 + jackpotCount * 20 && jackpotCount < 3 && addGold <= 100)
             {
                 jackpotRate += UnityEngine.Random.value;
                 if (jackpotRate >= jackpotCount + 1)
